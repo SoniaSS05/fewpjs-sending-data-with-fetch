8533779abc5ff925a45a943df6f19df606822016
Sonia <serrano_sonia@hotmail.com>
2021-04-11 19:23:50 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+
+
+
+    const submitData = function (name,email){ 
+      
+      const answer = fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email})
+      })
+
+      .then(resp => resp.json())
+      .then(json => showPage(json))
+      .catch(function(error) {
+        showPageError(error);
+        alert("Bad things!");
+        console.log(error.message);
+      });
+    return answer;
+    }
+
+
+    function showPage(idg){
+      const body = document.querySelector('body');
+      const h1 = document.createElement('h1');
+      h1.innerHTML = `Id:  ${idg.id}`;
+      body.appendChild(h1);
+    }
+
+    function showPageError(error){
+      const bodySelect = document.querySelector('body');
+      let lastLi = bodySelect.lastElementChild;
+      bodySelect.removeChild(lastLi);
+      const h1 = document.createElement('h1');
+      h1.innerHTML = error;
+      bodySelect.appendChild(h1);
+    }
+   
+  //const answerG=submitData('s','s@hotmail.com');
+
+
+
